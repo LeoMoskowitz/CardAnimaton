a16368268b454af2a4841e2a470f66ef4f00ad90
LeoMoskowitz <33180377+LeoMoskowitz@users.noreply.github.com>
2017-10-28 11:01:45 -0400

    Add files via upload
    Adding card animation files.

---
+++ main.js
@@ -0,0 +1,66 @@
+console.log("Hi")
+
+var cardNumbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var cardSuit = ["diamonds", "spades", "clubs", "hearts"];
+
+var Card = function(){
+	this.suit = document.createElement('img');
+	this.number = document.createElement('div');
+	this.init = function(){
+		var cardContDiv = document.createElement('div'),
+	flipDiv = document.createElement('div'),
+	frontDiv = document.createElement('div'),
+	backDiv = document.createElement('div'),
+	catDiv = document.createElement('div');
+	this.suit.src = "Images/hearts.svg";
+
+cardContDiv.className = 'card_container';
+flipDiv.className = 'flip';
+frontDiv.className = 'card_front';
+backDiv.className = 'card_back';
+catDiv.className = 'card_category';
+
+catDiv.appendChild(this.number);
+catDiv.appendChild(this.suit);
+frontDiv.appendChild(catDiv);
+flipDiv.appendChild(frontDiv);
+flipDiv.appendChild(backDiv);
+cardContDiv.appendChild(flipDiv);
+		cardContDiv.onclick = function(e){
+			console.log(e.currentTarget);
+			e.currentTarget.classList.toggle("flip_card");
+//			setTimeout(function(e){
+//				e.currentTarget.classList.remove("flip_card");
+//			}, 650)
+		}
+
+var cardDeck = document.getElementById("cardDeck")
+
+cardDeck.appendChild(cardContDiv);
+	}
+}
+
+for (var i = cardNumbers.length - 1; i >= 0; i--) {
+
+	for(var j = cardSuit.length - 1; j >= 0; j--){
+		var CardDeck = new Card();
+		CardDeck.init();
+		CardDeck.number.innerHTML = cardNumbers[i];
+		CardDeck.suit.src = "Images/" + cardSuit[j] + ".svg";
+	}
+}
+
+//var CardDeck = new Card();
+//CardDeck.init();
+//
+//var CardDeck2 = new Card();
+//CardDeck2.init();
+//CardDeck2.suit.src="Images/spades.svg"
+//
+//var CardDeck3 = new Card();
+//CardDeck3.init();
+//CardDeck3.suit.src="Images/diamonds.svg"
+//
+//var CardDeck4 = new Card();
+//CardDeck4.init();
+//CardDeck4.suit.src="Images/clubs.svg"
